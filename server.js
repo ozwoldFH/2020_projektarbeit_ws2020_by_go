30838d4a08f3ecd0a8a862a6a5b4ced079971ab5
George Oswald <george.oswald@edu.fh-joanneum.at>
2021-01-12 09:34:25 +0100

    added ejs example

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-import './config';
+//import config, { nodeEnv, logTest } from './config.js';
+//logTest('This is a test message');
+
+import config from "./config.js";
+import express from "express";
+import apiRouter from "./api/index.js";
+
+const server = express();
+
+server.set("view engine", "ejs");
+
+server.get("/", (req, res) => {
+    res.send("Hello Express");
+});
+
+server.use("/api", apiRouter);
+server.use(express.static("public"));
+
+server.listen(config.port, () => {
+    console.info("Server listening to port ", config.port);
+});
+
+
+
